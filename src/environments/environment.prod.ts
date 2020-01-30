981aa5f926bf777f1162dc062f21e1b4840ffd11
Alina Kuzovkova <alina.kuzzzovkova@gmail.com>
2020-01-30 11:56:31 +0200

    final fix for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  CLIENT_ID: '',
+  CLIENT_ID: '7419229e221547db8d7d795ff0988bf7',
   REDIRECT_URI: 'https://musify-player.herokuapp.com/',
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
