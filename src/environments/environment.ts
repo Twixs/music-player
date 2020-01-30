3e1dd317ea311b5071b1ce71df0d570812336e18
Alina Kuzovkova <alina.kuzzzovkova@gmail.com>
2020-01-30 11:27:07 +0200

    fix to use prod mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  CLIENT_ID: '7419229e221547db8d7d795ff0988bf7', // here should be your client id from registered application
+  CLIENT_ID: '', // here should be your client id from registered application
   REDIRECT_URI: 'http://localhost:4200/',
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
