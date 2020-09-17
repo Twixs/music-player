1c9577493c4986aad9f9c01782caa9938e777039
alina.kuzovkova <alina.kuzzzovkova@gmail.com>
2020-09-18 00:30:31 +0300

    Fix environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  CLIENT_ID: '',
+  REDIRECT_URI: '',
+  STORAGE_KEY: 'auth_token',
+  BASE_URL: 'https://api.spotify.com/v1',
+  AUTH_URL: 'https://accounts.spotify.com/authorize'
 };
